b1d56f5595a870ac9b6f81a8181cc403d6fc9740
EsdrasSantosDV <esdrassantosoliveiradeveloper@gmail.com>
2024-05-15 23:47:44 -0300

    feat:initial repo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ReplaySubject, Subject, connectable, delay, interval, of } from 'rxjs';
+import {
+  ReplaySubject,
+  Subject,
+  connectable,
+  delay,
+  interval,
+  of,
+  takeUntil,
+} from 'rxjs';
 import { CommonModule } from '@angular/common';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -12,12 +21,14 @@ export class AppComponent implements OnInit {
   observable$ = interval(1000);
 
   subject = new ReplaySubject<number>(1);
-  //ERA O QUE QUERIA, CONECTAR UM OBSERVABLE NUM
-  multicasted = connectable(this.observable$, {
+  //ERA O QUE QUERIA, CONECTAR UM OBSERVABLE NUM SUBJECT- MOSTRAR ISSO PRO GIL DEPOIS
+  multicasted = connectable(this.observable$.pipe(takeUntilDestroyed()), {
     connector: () => this.subject,
   }).connect();
 
   ngOnInit(): void {
     this.subject.subscribe((value) => console.log(value));
   }
+
+
 }
